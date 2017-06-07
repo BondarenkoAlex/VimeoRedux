39eb35ffcf2e7c3751b3181320819828c0e17d38
Bondiki <bondarenkoalax@mail.ru>
2017-06-07 23:35:08 +0300

    tests conf

--- tests/client/browser/src/hello.spec.js
+++ tests/client/browser/src/hello.spec.js
@@ -1,12 +1,14 @@
-describe('hello', function(){
-	debugger;
+describe('hello', function () {
+  debugger;
 
-    it('should say hello', function(){
-        //expect(hello()).to.be('hello world');
-        assert.ok(hello(), 'hello world');
-    });
+  it('should say hello', function () {
+    //expect(hello()).to.be('hello world');
+    assert.ok(hello(), 'hello world');
+  });
 
-    it('should say hello to person', function(){
-        expect(hello('Bob')).to.equal('hello Bob');
-    });
+  it('should say hello to person', function () {
+    expect(hello('Bob'))
+      .to
+      .equal('hello Bob');
+  });
 });
