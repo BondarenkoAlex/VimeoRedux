7d4d8df2f629fa91886ba172298b7126c642600e
Bondiki <bondarenkoalax@mail.ru>
2017-07-16 22:41:07 +0300

    realisation view(update): categiries, subcategories, playlist

--- src/client/browser/src/routes.js
+++ src/client/browser/src/routes.js
@@ -5,47 +5,90 @@ import CategoryContainer from './containers/ContentContainer/CategoryContainer';
 import SubcategoryContainer from './containers/ContentContainer/SubcategoryContainer';
 import PlaylistContainer from './containers/ContentContainer/PlaylistContainer';
 import VideoContainer from './containers/ContentContainer/VideoContainer';
-//import PlayerBreadContentLayout from './components/layout/PlayerBreadContentLayout';
+
+const project = require('../../../../webpack/project.config.js');
+const publicPath = project.publicPath;
 
 const routes = [{
-    component: AppLayout,
+  component: AppLayout,
+  routes: [{
+    path: `${publicPath}`,
+    exact: true,
+    component: CategoryContainer,
+  }, {
+    path: `${publicPath}search`,
+    exact: true,
+    component: null,
     routes: [{
-        path: '/',
-        exact: true,
-        component: CategoryContainer,
-    }, {
-        path: '/search',
-        exact: true,
-        component: null,
-        routes: [{
-            path: '/search/????',
-            component: null,
-          //можно подключать разные контейнеры и спускать в компоненты нужные данные
-          // из раутера (Route) мы коннектимся к нужным данным и спускаем дальше
-        }, ],
-    }, {
-        path: '/favorites',
-        component: null,
-        routes: [{
-            path: '/favorites/????',
-            component: null,
-        }, ],
-    },{
-        path: '/:category/:subcategory/:idVideo',
-        exact: true,
-        component: VideoContainer,
-        routes: [],
-    },{
-        path: '/:category/:subcategory',
-        exact: true,
-        component: PlaylistContainer,
-        routes: [],
-      },{
-        path: '/:category',
-        exact: true,
-        component: SubcategoryContainer,
-        routes: [],
-    }, ],
-}, ];
+      path: `${publicPath}search/????`,
+      component: null,
+      //можно подключать разные контейнеры и спускать в компоненты нужные данные
+      // из раутера (Route) мы коннектимся к нужным данным и спускаем дальше
+    },],
+  }, {
+    path: `${publicPath}favorites`,
+    component: null,
+    routes: [{
+      path: `${publicPath}favorites/????`,
+      component: null,
+    },],
+  }, {
+    path: `${publicPath}:category/:subcategory/:idVideo`,
+    exact: true,
+    component: VideoContainer,
+    routes: [],
+  }, {
+    path: `${publicPath}:category/:subcategory`,
+    exact: true,
+    component: PlaylistContainer,
+    routes: [],
+  }, {
+    path: `${publicPath}:category`,
+    exact: true,
+    component: SubcategoryContainer,
+    routes: [],
+  },],
+},];
+
+// const routes = [{
+//   component: AppLayout,
+//   routes: [{
+//     path: '/',
+//     exact: true,
+//     component: CategoryContainer,
+//   }, {
+//     path: '/search',
+//     exact: true,
+//     component: null,
+//     routes: [{
+//       path: '/search/????',
+//       component: null,
+//       //можно подключать разные контейнеры и спускать в компоненты нужные данные
+//       // из раутера (Route) мы коннектимся к нужным данным и спускаем дальше
+//     },],
+//   }, {
+//     path: '/favorites',
+//     component: null,
+//     routes: [{
+//       path: '/favorites/????',
+//       component: null,
+//     },],
+//   }, {
+//     path: '/:category/:subcategory/:idVideo',
+//     exact: true,
+//     component: VideoContainer,
+//     routes: [],
+//   }, {
+//     path: '/:category/:subcategory',
+//     exact: true,
+//     component: PlaylistContainer,
+//     routes: [],
+//   }, {
+//     path: '/:category',
+//     exact: true,
+//     component: SubcategoryContainer,
+//     routes: [],
+//   },],
+// },];
 
 export { routes as default };
