ee8963689ef12d3efbad064eb068bbe656f1839c
Alexey Bondarenko <bondarenko@banki.ru>
2018-04-06 18:08:10 +0300

    commit

---
+++ webpack/loaders/scss-module-loader.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: process.env.NODE_ENV === 'development'
+    ? 'styles/[name].[contenthash].css'
+    : 'styles/[name].css',
+  allChunks: true,
+});
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: extractSass.extract({
+        use: [{
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[name]__[local]--[hash:base64:3]'
+            // localIdentName: '[path][name]__[local]--[hash:base64:5]'
+          }
+        }, {
+          loader: 'sass-loader',
+        },],
+        // use style-loader in development
+        fallback: 'style-loader',
+      }),
+    },],
+  },
+  plugins: [
+    extractSass,
+  ],
+};
